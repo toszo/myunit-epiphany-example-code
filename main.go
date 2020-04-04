89cdbf367aceca4c40d290988fb433db46efdc66
pprach <pawel@prach.pl>
2020-04-04 14:32:19 +0200

    Change api design

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func handlerCheck(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Ok")
+    fmt.Fprintf(w, "golang")
 }
 
 func handlerHost(w http.ResponseWriter, r *http.Request) {
@@ -21,11 +21,15 @@ func handlerHost(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func handlerLastValue(w http.ResponseWriter, r *http.Request) {
+func handlerNumber(w http.ResponseWriter, r *http.Request) {
     r.ParseForm()
-    fmt.Println(r.Form)
-    ret, _ := strconv.Atoi(r.Form["number"][0])
-    ret*=2
+    var ret int
+    if r.Form["number"] != nil {
+        ret, _ = strconv.Atoi(r.Form["number"][0])
+        ret*=2
+    } else {
+        ret = 0
+    }
     fmt.Fprintf(w, strconv.Itoa(ret))
 }
 
@@ -33,6 +37,6 @@ func main() {
     http.HandleFunc("/check", handlerCheck)
     http.HandleFunc("/status", handlerCheck)
     http.HandleFunc("/host", handlerHost)
-    http.HandleFunc("/last", handlerLastValue)
+    http.HandleFunc("/number", handlerNumber)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
